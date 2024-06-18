e9a591d4e3907d4dd4dad9c8a61cf3ae80ab71bc
Yunus Emre <kahraman03040506@hotmail.com>
2024-06-18 03:23:09 +0300

    VirtualMethods

---
+++ BasicConcepts/VirtualMethods/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace VirtualMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SqlServer sqlServer = new SqlServer();
+        sqlServer.Add();
+        
+        MySql mySql = new MySql();
+        mySql.Add();
+    }
+
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by Default");
+        }
+
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by Default");
+        }
+    }
+
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql Code");
+            //base.Add();
+        }
+    }
+
+    class MySql : Database
+    {
+        
+    }
+}
