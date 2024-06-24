82b0d1fc7770b96ca325b5ba352816ce85ee03f7
Yunus Emre <kahraman03040506@hotmail.com>
2024-06-24 04:00:25 +0300

    Recap Demo

---
+++ BasicConcepts/RecapDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace RecapDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Logger = new DatabaseLogger();
+        customerManager.Add();
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer Added");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Database!");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to File!");
+        }
+    }
+}
