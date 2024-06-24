8340f136419b2243a8c9cd46ecc9206f1cf42c0d
Yunus Emre <kahraman03040506@hotmail.com>
2024-06-24 11:05:09 +0300

    Constructors

---
+++ BasicConcepts/AccesModifiers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AccesModifiers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Customer
+{
+    private int id;
+
+    public void Save()
+    {
+        
+    }
+}
+
+class Student
+{
+    public void Save()
+    {
+        Customer customer = new Customer();
+        customer.S
+    }
+}
