a50f778d7a05d045f77ea45f694c549de1ea36c7
Yunus Emre <kahraman03040506@hotmail.com>
2024-06-24 03:46:43 +0300

    AbstractClasses

---
+++ BasicConcepts/AbstractClasses/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AbstractClasses;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Database dataBaseSql = new SqlServer();
+        Database dataBaseOracle = new Oracle();
+        
+        dataBaseSql.Deleted();
+        dataBaseOracle.Deleted();
+    }
+    
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Default Added");
+        }
+
+        public abstract void Deleted();
+    }
+
+    class SqlServer : Database
+    {
+        public override void Deleted()
+        {
+            Console.WriteLine("Sql Added");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Deleted()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+    }
+}
