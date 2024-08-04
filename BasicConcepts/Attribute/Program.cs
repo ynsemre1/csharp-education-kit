a6018b41aa9b3f850833424ad63b18c8c318d786
Yunus Emre <kahraman03040506@hotmail.com>
2024-08-05 01:28:34 +0300

    Attributes

---
+++ BasicConcepts/Attribute/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Attribute;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    
+}
---
+++ BasicConcepts/Attributes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Attributes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer customer = new Customer { Id = 1, LastName = "Emre", Age = 21 };
+        CustomerDal customerDal = new CustomerDal();
+        customerDal.Add(customer);
+    }
+}
+
+[ToTable("Customers")]
+[ToTable("CustomersTbl")]
+class Customer
+{
+    public int Id { get; set; }
+    [RequiredProperty]
+    public string? FirstName { get; set; }
+    [RequiredProperty]
+    public string? LastName { get; set; }
+    [RequiredProperty]
+    public int Age { get; set; }
+}
+
+class CustomerDal
+{
+    [Obsolete("Dont use this Method")]
+    public void Add(Customer customer)
+    {
+        Console.WriteLine("{0},{1},{2},{3} added!", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+    }
+    
+    public void AddNew(Customer customer)
+    {
+        
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+class RequiredPropertyAttribute:Attribute
+{
+    
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+class ToTableAttribute:Attribute
+{
+    private string _tableName;
+
+    public ToTableAttribute(string tableName)
+    {
+        _tableName = tableName;
+    }
+}
