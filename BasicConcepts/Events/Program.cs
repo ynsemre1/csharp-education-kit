79f4981d19a6b592b182bf42e4a76b956293c56f
Yunus Emre <kahraman03040506@hotmail.com>
2024-08-10 05:21:21 +0300

    Events

---
+++ BasicConcepts/Events/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product hardDisk = new Product(50);
+        hardDisk.ProductName = "Hard Disk";
+
+        Product gsm = new Product(50);
+        gsm.ProductName = "GSM";
+        gsm.StockControlEvent += GsmOnStockControlEvent;
+
+        for (int i = 0; i < 10; i++)
+        {
+            hardDisk.Sell(10);
+            gsm.Sell(10);
+            Console.ReadLine();
+        }
+    }
+
+    private static void GsmOnStockControlEvent()
+    {
+        Console.WriteLine("GSM about to finish!!!");
+    }
+}
