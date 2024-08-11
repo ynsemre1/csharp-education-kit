37c887b3a5770b86142ba9cf8894e3e2c2813556
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-12 02:51:46 +0300

    LayerArchitecture

---
+++ NLayeredAppDemo/Nothwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.DataAccess.Concrete;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nothwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+
+        public List<Product> GetAll()
+        {
+            //Bussines Code
+            return _productDal.GetAll();
+        }
+    }
+}
