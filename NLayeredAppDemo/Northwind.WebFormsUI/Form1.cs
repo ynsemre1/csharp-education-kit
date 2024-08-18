9d4ebba24ba8008c1fd4e248335e7a7ec00e94d3
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-19 02:19:55 +0300

    NLayeredApp Dependency Incejtion with Ninject

--- NLayeredAppDemo/Northwind.WebFormsUI/Form1.cs
+++ NLayeredAppDemo/Northwind.WebFormsUI/Form1.cs
@@ -13,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Northwind.Entities.Concrete;
+using Nothwind.Business.DependencyResolvers.Ninject;
 
 namespace Northwind.WebFormsUI
 {
@@ -24,8 +25,8 @@ namespace Northwind.WebFormsUI
         public Form1()
         {
             InitializeComponent();
-            _productService = new ProductManager(new EfProductDal());
-            _categoryService = new CategoryManager(new EfCategoryDal());
+            _productService = InstanceFactory.GetInstance<IProductService>();
+            _categoryService = InstanceFactory.GetInstance<ICategoryService>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
---
+++ NLayeredAppDemo/Nothwind.Business/DependencyResolvers/Ninject/BussinesModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Nothwind.Business.Abstract;
+using Nothwind.Business.Concrete;
+
+namespace Nothwind.Business.DependencyResolvers.Ninject
+{
+    public class BussinesModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+
+            Bind<ICategoryService>().To<CategoryManager>().InSingletonScope();
+            Bind<ICategoryDal>().To<EfCategoryDal>().InSingletonScope();
+        }
+    }
+}
