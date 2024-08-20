815600470bff18f01c4c88badd1c6904a7a989e1
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-20 07:45:18 +0300

    Facade Pattern

---
+++ DesingPatterns/Facade/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ILogging
+    {
+        void Log();
+    }
+
+    class Logging :ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    interface ICaching
+    {
+        void Cache();
+    }
+
+    class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User Checkted");
+        }
+    }
+
+    class CustomerManager
+    {
+        private CrossCuttingConcernsFacade _concerns;
+
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _concerns.Caching.Cache();
+            _concerns.Authorize.CheckUser();
+            _concerns.Logging.Log();
+            Console.WriteLine("Saved");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+        }
+    }
+}
