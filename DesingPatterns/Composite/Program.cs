f23a76e92bbf7b43a98561c602dd361bcac5ed5f
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-20 12:28:10 +0300

    Composite Pattern

---
+++ DesingPatterns/Composite/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee yunus = new Employee{Name = "Yunus"};
+            Employee emre = new Employee { Name = "Emre" };
+            Employee yagmur = new Employee { Name = "Yagmur" };
+            Employee engin = new Employee { Name = "Engin" };
+
+            Contractor ali = new Contractor { Name = "Ali" };
+
+            yagmur.AddSubordinate(ali);
+
+            yunus.AddSubordinate(emre);
+            yunus.AddSubordinate(yagmur);
+            emre.AddSubordinate(engin);
+
+            Console.WriteLine(yunus.Name);
+            foreach (Employee manager in yunus)
+            {
+                Console.WriteLine("  {0}",manager.Name);
+
+                foreach (IPerson employee in manager)
+                {
+                    Console.WriteLine("    {0}", employee.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    interface IPerson
+    {
+        string Name { get; set; }
+    }
+
+    class Contractor : IPerson
+    {
+        public string Name { get; set; }
+    }
+
+    class Employee : IPerson, IEnumerable<IPerson>
+    {
+        List<IPerson> _subordinates = new List<IPerson>();
+
+        public void AddSubordinate(IPerson person)
+        {
+            _subordinates.Add(person);
+        }
+
+        public void RemoveSubordinate(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+
+        public IPerson GetSubOrdinate(int index)
+        {
+            return _subordinates[index];
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                yield return subordinate;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
