6728d7b6a91f61aa38257582fd301b4dc792c22d
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-21 09:45:44 +0300

    Observer Pattern

---
+++ DesingPatterns/Observer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerObserver = new CustomerObserver();
+            ProductManager productManager = new ProductManager();
+            productManager.Attach(customerObserver);
+            productManager.Attach(new EmployeeObserver());
+            productManager.Detach(customerObserver);
+            productManager.UpdatePrice();
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> _observers = new List<Observer>();
+
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Product Price Changed");
+            Notify();
+        }
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to Customer : Product Price Changed!");
+        }
+    }
+
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to Employee : Product Price Changed!");
+        }
+    }
+}
