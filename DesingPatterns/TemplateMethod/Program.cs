343cb80151498de0b551091483cad90a4ab33539
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-22 04:32:32 +0300

    TemplateMethod Pattern

---
+++ DesingPatterns/TemplateMethod/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ScroingAlgorithm algorithm;
+            
+            Console.WriteLine("Mans");
+            algorithm = new ManScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(8, new TimeSpan(0,2,34)));
+
+            Console.WriteLine("Women");
+            algorithm = new WomenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(8, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("Children");
+            algorithm = new ChilderenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(8, new TimeSpan(0, 2, 34)));
+
+            Console.ReadLine();
+        } 
+    }
+
+    public abstract class ScroingAlgorithm
+    {
+        public int GenerateScore(int hits, TimeSpan time)
+        {
+            int score = CalculateBaseScore(hits);
+            int reduction = CalculateReduction(time);
+
+            return CalculateOverallScore(score, reduction);
+        }
+
+        public abstract int CalculateOverallScore(int score, int reduction);
+
+        public abstract int CalculateReduction(TimeSpan time);
+        
+        public abstract int CalculateBaseScore(int hits);
+    }
+
+    class ManScoringAlgorithm : ScroingAlgorithm
+    {
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 2;
+        }
+
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 70;
+        }
+    }
+
+    class WomenScoringAlgorithm : ScroingAlgorithm
+    {
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 3;
+        }
+
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+    }
+
+    class ChilderenScoringAlgorithm : ScroingAlgorithm
+    {
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 5;
+        }
+
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 120;
+        }
+    }
+}
