25a71d1faf82caef60daf828ee28f909a0c41369
Yunus Emre <yunusemreakyol000@gmail.com>
2024-08-23 06:23:25 +0300

    Mediator Pattern

---
+++ DesingPatterns/Mediator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mediator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+
+            Teacher yunus = new Teacher(mediator);
+            yunus.Name = "Yunus";
+
+            mediator.Teacher = yunus;
+
+            Student yagmur = new Student(mediator);
+            yagmur.Name = "Yagmur";
+
+            Student sena = new Student(mediator);
+            sena.Name = "Sena";
+
+            mediator.Students = new List<Student>{sena, yagmur};
+
+            yunus.SendNewImageUrl("slide1.jpg");
+            yunus.RecieveQuestion("is it true?", sena);
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CourseMember
+    {
+        protected Mediator Mediator;
+        public CourseMember(Mediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+
+    class Teacher : CourseMember
+    {
+        public string Name { get; set; }
+        public Teacher(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void RecieveQuestion(string question, Student student)
+        {
+            Console.WriteLine("Teacher recieved a Question from {0}, {1}", student.Name, question);
+        }
+
+        public void SendNewImageUrl(string url)
+        {
+            Console.WriteLine("Teacher changed slide: {0}", url);
+            Mediator.UpdateImage(url);
+        }
+
+        public void AnswerQuestion(string answer, Student student)
+        {
+            Console.WriteLine("Teacher answered question {0},{1}", student.Name, answer);
+        }
+    }
+
+    class Student : CourseMember
+    {
+        public string Name { get; set; }
+        public Student(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void RecieveImage(string url)
+        {
+            Console.WriteLine("{1} Student received image: {0}", url, Name);
+        }
+
+        public void ReciveAnswer(string answer)
+        {
+            Console.WriteLine("{1} Student received answer {0}", answer, Name);
+        }
+    }
+
+    class Mediator
+    {
+        public Teacher Teacher { get; set; }
+        public List<Student> Students { get; set; }
+
+        public void UpdateImage(string url)
+        {
+            foreach (var student in Students)
+            {
+                student.RecieveImage(url);
+            }
+        }
+
+        public void SendQuestion(string question, Student student)
+        {
+            Teacher.RecieveQuestion(question, student);
+        }
+
+        public void SendAnswer(string answer, Student student)
+        {
+            student.ReciveAnswer(answer);
+        }
+    }
+}
